941131df10989bbbaddffff17457296658dc41de
Constantin Runtov <ghostshow@yandex.ru>
2021-03-12 12:10:28 +0200

    Little code reform
    Signed-off-by: Constantin Runtov <ghostshow@yandex.ru>

--- ImapClient/ImapClient.py
+++ ImapClient/ImapClient.py
@@ -1,10 +1,15 @@
 import os
+from typing import List
 
 import imapclient
 import pyzmail
+
 from Logger.Logger import Log
 import sentry_sdk
-sentry_sdk.init("https://eca61270fe5e4ceeb1046ad58ad7333e@o402810.ingest.sentry.io/5264523")
+
+sentry_sdk.init(
+    "https://eca61270fe5e4ceeb1046ad58ad7333e@o402810.ingest.sentry.io/5264523"
+)
 
 
 class ImapClient:
@@ -13,70 +18,88 @@ class ImapClient:
     """
 
     def __init__(self):
-        self.imap_link = os.getenv("Mail_Link")
-        self.mail_login = os.getenv("Mail_Login")
-        self.mail_password = os.getenv("Mail_Password")
-        self.final_list_to_send = []
-        self.log_file = Log(self.__class__)
+        self.__imap_link: str = os.getenv("Mail_Link")
+        self.__mail_login: str = os.getenv("Mail_Login")
+        self.__mail_password: str = os.getenv("Mail_Password")
+        self.__final_list_to_send: List[dict] = []
+        self.__log_file: Log = Log(self.__class__)
 
-    def get_all_messages(self):
+    def __get_all_messages(self):
+        # type: (ImapClient) -> None
         """
         Fetch,parse and add the parsed message with his (uid, email_info) to list of messages from a selected folder
         :return:
         """
 
-        self.final_list_to_send.clear()
+        self.__final_list_to_send.clear()
 
         try:
-            with imapclient.IMAPClient(self.imap_link, 993) as server:
-                server.login(self.mail_login, self.mail_password)
+            with imapclient.IMAPClient(self.__imap_link, 993) as server:
+                server.login(self.__mail_login, self.__mail_password)
                 server.select_folder("INBOX")
 
-                for uid, msg in server.fetch(server.search(), 'RFC822').items():
-                    message_factory = pyzmail.PyzMessage.factory(msg[b'RFC822'])  # Be sure to change the uid num
-                    content_type = message_factory.smart_parser(message_factory).get_content_type()
-                    msg_text = ""
+                for uid, msg in server.fetch(server.search(), "RFC822").items():
+                    # Be sure to change the uid num
+                    message_factory: pyzmail.PyzMessage = pyzmail.PyzMessage.factory(
+                        msg[b"RFC822"]
+                    )
+                    content_type: str = message_factory.smart_parser(
+                        message_factory
+                    ).get_content_type()
+                    msg_text: str = ""
                     if message_factory.text_part is None:
                         msg_text += "No text provided :-("
                     else:
-                        msg_text_part = message_factory.text_part.get_payload().decode(
-                            message_factory.text_part.charset
-                        ).splitlines()
-                        for i in range(0, len(msg_text_part) - 1):
+                        msg_text_part = (
+                            message_factory.text_part.get_payload()
+                            .decode(message_factory.text_part.charset)
+                            .splitlines()
+                        )
+                        for idx in range(0, len(msg_text_part) - 1):
                             try:
-                                if msg_text_part[i + 1].startswith("От:") \
-                                        or msg_text_part[i+1].startswith("> От:") \
-                                        or msg_text_part[i + 1].startswith("From:") \
-                                        or msg_text_part[i + 1].__contains__(
-                                    f"<{message_factory.smart_parser(message_factory).get('Delivered-To')}>") \
-                                        or msg_text_part[i + 1].__contains__("[image: ") \
-                                        or msg_text_part[i+1].startswith("Отправлено с iPhone") \
-                                        or msg_text_part[i+1].__contains__("Начало переадресованного сообщения"):
-                                    msg_text += msg_text_part[i] + " "
+                                if (
+                                    msg_text_part[idx + 1].startswith("От:")
+                                    or msg_text_part[idx + 1].startswith("> От:")
+                                    or msg_text_part[idx + 1].startswith("From:")
+                                    or msg_text_part[idx + 1].__contains__(
+                                        f"<{message_factory.smart_parser(message_factory).get('Delivered-To')}>"
+                                    )
+                                    or msg_text_part[idx + 1].__contains__("[image: ")
+                                    or msg_text_part[idx + 1].startswith(
+                                        "Отправлено с iPhone"
+                                    )
+                                    or msg_text_part[idx + 1].__contains__(
+                                        "Начало переадресованного сообщения"
+                                    )
+                                ):
+                                    msg_text += msg_text_part[idx] + " "
                                     break
                                 else:
-                                    msg_text += msg_text_part[i] + " "
+                                    msg_text += msg_text_part[idx] + " "
                             except IndexError as index_error:
-                                self.log_file.log_all(2, index_error)
+                                self.__log_file.log_all(2, index_error)
 
-                    final_mail_inf = "You Have A New Email In The Mailbox:\n" \
-                                     f"From: {message_factory.smart_parser(message_factory).get('From')}\n" \
-                                     f"Subject: {message_factory.get_subject()}\n" \
-                                     f"Text: {msg_text if msg_text else 'No'}\n" \
-                                     f"Attachments: {'Yes' if content_type == 'multipart/mixed' or 'multipart/related' else 'No'} "
-                    self.final_list_to_send.append({
-                        "uid": uid,
-                        "msg_text": final_mail_inf
-                    })
+                    final_mail_inf: str = (
+                        "You Have A New Email In The Mailbox:\n"
+                        f"From: {message_factory.smart_parser(message_factory).get('From')}\n"
+                        f"Subject: {message_factory.get_subject()}\n"
+                        f"Text: {msg_text if msg_text else 'No'}\n"
+                        f"Attachments: {'Yes' if content_type == ('multipart/mixed' or 'multipart/related') else 'No'} "
+                    )
+                    self.__final_list_to_send.append(
+                        {"uid": uid, "msg_text": final_mail_inf}
+                    )
         except imapclient.IMAPClient.Error as imap_error:
-            self.log_file.log_all(1, str(imap_error))
-            self.get_all_messages()
+            self.__log_file.log_all(1, str(imap_error))
+            self.__get_all_messages()
 
-    def get_array_of_messages_to_send(self) -> list:
+    def get_array_of_messages_to_send(self):
+        # type: (ImapClient) -> List[dict]
         """
         Getting messages with a flag ['All'] from selected mail folder
         :return :list of tuples (uid, email_info) get from selected mail folder
         """
 
-        self.get_all_messages()
-        return self.final_list_to_send
+        self.__get_all_messages()
+        self.__final_list_to_send.reverse()
+        return self.__final_list_to_send
--- VkNotifier/VkNotifier.py
+++ VkNotifier/VkNotifier.py
@@ -1,11 +1,13 @@
+from typing import List
+from time import sleep
+import os
+from datetime import datetime
+
 import vk_api
 from vk_api.utils import get_random_id
 from Logger.Logger import Log
 from ImapClient.ImapClient import ImapClient
 from SqlModule.SqlLiteModule import SqlLiteModule
-from time import sleep
-import os
-from datetime import datetime
 import sentry_sdk
 
 sentry_sdk.init(
@@ -18,7 +20,8 @@ class VkNotifier:
     Class representing the VK bot
     """
 
-    def __init__(self, login_method: int, chat_to_send: int, send_only_to_dev: bool):
+    def __init__(self, login_method, chat_to_send, send_only_to_dev=True):
+        # type: (VkNotifier, int, int, bool) -> None
         self.user_login: str = ""
         self.user_password: str = ""
         self.token: str = os.getenv("Vk_Token")
@@ -32,12 +35,13 @@ class VkNotifier:
         self.group_chat_id: int = self.group_chat_ids.get(chat_to_send)
         self.log_file: Log = Log(class_name=self.__class__)
         self.mail_client: ImapClient = ImapClient()
-        self.new_inbox_email: list = []
+        self.new_inbox_email: List[dict] = []
         self.counter: int = 0
         self.db: SqlLiteModule = SqlLiteModule()
         self.db.check_table()
 
-    def check_for_new_messages(self) -> None:
+    def check_for_new_messages(self):
+        # type: (VkNotifier) -> None
         """
         Checking the mailbox for new messages. If found unseen for the bot,
         then notify chat and dev about this message by sending message text of unseen message
@@ -45,16 +49,17 @@ class VkNotifier:
         :return: None
         """
 
-        new_messages_sent = False
-        self.new_inbox_email = self.mail_client.get_array_of_messages_to_send()
-        self.new_inbox_email.reverse()
+        new_messages_sent: bool = False
+        self.new_inbox_email: List[
+            dict
+        ] = self.mail_client.get_array_of_messages_to_send()
         if not self.new_inbox_email:
             self.no_new_messages()
         else:
-            for item in self.new_inbox_email:
-                if self.db.check_for_message(item["msg_text"]) == 0:
-                    self.notify(item["msg_text"])
-                    self.db.add_message(item["uid"], item["msg_text"])
+            for msg in self.new_inbox_email:
+                if self.db.check_for_message(message_text=msg["msg_text"]) == 0:
+                    self.notify(text=msg["msg_text"])
+                    self.db.add_message(uid=msg["uid"], message_text=msg["msg_text"])
                     new_messages_sent = True
             if new_messages_sent:
                 self.log_file.log_all(3, "Found unseen messages")
@@ -63,7 +68,8 @@ class VkNotifier:
             else:
                 self.no_new_messages()
 
-    def no_new_messages(self) -> None:
+    def no_new_messages(self):
+        # type: (VkNotifier) -> None
         """
         Log and send a message to dev if no new messages have been found
         :return:
@@ -75,15 +81,17 @@ class VkNotifier:
             self.send_message_to_dev("No unseen messages\nGoing to sleep :-(")
             self.reset_counter()
 
-    def reset_counter(self) -> None:
+    def reset_counter(self):
+        # type: (VkNotifier) -> None
         """
         Reset counter of No unseen messages log
         :return:
         """
 
-        self.counter = 0
+        self.counter: int = 0
 
-    def notify(self, text: str) -> None:
+    def notify(self, text):
+        # type: (VkNotifier, str) -> None
         """
         Sending a message to selected chat and dev
         :param text: message text
@@ -102,7 +110,8 @@ class VkNotifier:
             self.login_throw_group()
             self.notify(text)
 
-    def login_throw_user(self) -> None:
+    def login_throw_user(self):
+        # type: (VkNotifier) -> None
         """
         Login to vk throw user account
         :return:
@@ -114,7 +123,8 @@ class VkNotifier:
         except vk_api.AuthError as error_msg:
             self.log_file.log_all(1, error_msg)
 
-    def login_throw_group(self) -> None:
+    def login_throw_group(self):
+        # type: (VkNotifier) -> None
         """
         Login to vk through group bot token
         :return:
@@ -123,7 +133,8 @@ class VkNotifier:
         self.vk_session = vk_api.VkApi(token=self.token)
         self.vk = self.vk_session.get_api()
 
-    def send_message_to_dev(self, text: str) -> None:
+    def send_message_to_dev(self, text):
+        # type: (VkNotifier, str) -> None
         """
         Sending message to dev
         :param text: message text
@@ -134,7 +145,8 @@ class VkNotifier:
             user_id=self.dev_vk_id, random_id=get_random_id(), message=text
         )
 
-    def send_message_to_chat(self, text: str) -> None:
+    def send_message_to_chat(self, text):
+        # type: (VkNotifier, str) -> None
         """
         Sending message to selected chat
         :param text: message text
@@ -142,7 +154,7 @@ class VkNotifier:
         """
 
         self.vk.messages.send(
-            peer_id="200000000{}".format(self.group_chat_id),
+            peer_id="200000000{0}".format(self.group_chat_id),
             random_id=get_random_id(),
             message=text,
         )
